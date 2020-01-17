4579aabd0435d535760739c69f97ca3939c129df
Rodrigo Varga <varguiniano@gmail.com>
2020-01-17 08:50:38 +0100

    - Added some scripts with a Readme to see if Unity recognizes it as a package on the server now.

---
+++ Editor/Readme.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Packages.ModelOutline.Editor
+{
+    /// <summary>
+    /// Simple readme class.
+    /// </summary>
+    public class Readme : ScriptableObject
+    {
+        
+    }
+}
---
+++ Editor/ReadmeEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+
+namespace Packages.ModelOutline.Editor
+{
+    /// <summary>
+    /// Custom editor for the readme asset.
+    /// </summary>
+    [CustomEditor(typeof(Readme))]
+    public class ReadmeEditor : UnityEditor.Editor
+    {
+        /// <summary>
+        /// Paint the UI.
+        /// </summary>
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.LabelField("This package provides a simple outline using a custom forward renderer");
+            EditorGUILayout.LabelField("with additional render features.");
+            EditorGUILayout.LabelField("By default it draws an outline on the objects that are on layer 8. We usually call it \"Outline\".");
+            EditorGUILayout.LabelField("You can either use the custom renderer or create one of your own copying");
+            EditorGUILayout.LabelField("the one provided so that you can modify it.");
+            EditorGUILayout.LabelField("Doing this will also allow you yo change the material for the outline,");
+            EditorGUILayout.LabelField("choosing a different color or width.");
+        }
+    }
+}
